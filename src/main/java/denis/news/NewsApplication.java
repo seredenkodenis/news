5b73b639ebc4d901fa376f507a1440204e86a597
mvoronoy <mvoronoy@gmail.com>
2018-12-23 15:44:18 +0200

    Реконфигурация сборочных скриптов

--- src/main/java/denis/news/NewsApplication.java
+++ src/main/java/denis/news/NewsApplication.java
@@ -1,9 +1,16 @@
 package denis.news;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+//@EnableAutoConfiguration
+@ComponentScan({"denis.news"})
+@EnableJpaRepositories
 public class NewsApplication {
 
     public static void main(String[] args) {
